976bbd8906809f248fd6759f29fe4305942916e0
pace11 <ryanjoker87@gmail.com>
2024-08-19 20:25:14 +0700

    feat: tugas-1 component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import Button from "@/components/button";
+import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -106,6 +107,7 @@ export default function Home() {
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
             </p>
+            <Button />
           </a>
         </div>
       </main>
